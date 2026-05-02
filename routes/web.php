3b4580cdc08dfafb88c38c9794278fd0966f7177
Syahdan Faisal Akbar <syahdanfaisalakbar@gmail.com>
2026-05-02 16:01:10 +0700

    update category API

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductWebController;
 
 Route::get('/', function () {
     return view('welcome');
